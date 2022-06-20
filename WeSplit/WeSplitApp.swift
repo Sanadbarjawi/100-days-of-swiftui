2670851d63c30a3ceddfdd7a1408d5c16b564250
Sanad Barjawi <sanad.barjawi@careem.com>
2022-06-20 08:57:23 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sanad Barjawi on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

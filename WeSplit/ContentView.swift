dd42e09697d9ca256ccf712363dd8ec0cafed211
Sanad Barjawi <sanad.barjawi@careem.com>
2022-06-24 09:57:49 +0300

    UI implementation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var amount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentages: [Int] = [15,20,25]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("amount", value: $amount, format: .currency(code: "USD"))
+                    Picker("Numbe of People", selection: $numberOfPeople) {
+                        ForEach(0..<10) { num in
+                            Text("\(num)")
+                        }
+                    }.pickerStyle(.automatic)
+                }
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip Percentage", selection: $tipPercentages) {
+                        ForEach(tipPercentages, id: \.self) { tip in
+                            Text(tip, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }.navigationTitle("We Split")
+        }
     }
 }
 

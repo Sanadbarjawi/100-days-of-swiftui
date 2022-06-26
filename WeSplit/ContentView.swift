22db4d004e85621be70642b90217d1008187ccc5
Sanad Barjawi <sanad.barjawi@careem.com>
2022-06-26 18:20:44 +0300

    total amount per person logic

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var amount: Double?
+    @State private var amount: Double
     @State private var selectedNumberOfPeople: Int
     @State private var selectedTipPercentage: Int
     
     private var numberOfPeopleRange = 1...100
     private var tipPercentages: [Int] = [10,15,20,25]
 
+    private var totalAmount: Double {
+        let peopleCount = Double(selectedNumberOfPeople)
+        let tipSelection = Double(selectedTipPercentage)
+        let tipValue = amount / 100 * tipSelection
+        let total = tipValue / peopleCount
+        return total
+    }
+    
     init() {
         _selectedNumberOfPeople = .init(initialValue: numberOfPeopleRange.lowerBound)
         _selectedTipPercentage = .init(initialValue: tipPercentages.first!)
+        _amount = .init(initialValue: 0.0)
     }
 
     var body: some View {
@@ -35,13 +44,17 @@ struct ContentView: View {
                 Section {
                     Picker("Tip Percentage", selection: $selectedTipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
-                            Text("\($0)")
+                            Text($0, format: .percent)
                         }
                     }.pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
-            }.navigationTitle("We Split")
+                Section {
+                    Text(totalAmount, format: .currency(code: "USD"))
+                }
+            }
+            .navigationTitle("We Split")
         }
     }
 }

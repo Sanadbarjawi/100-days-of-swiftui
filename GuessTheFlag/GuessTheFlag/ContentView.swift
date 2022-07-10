e862b3e10885c19bd6f790110f860c5fc32fe087
Sanad Barjawi <sanad.barjawi@careem.com>
2022-07-10 18:11:03 +0300

    Stacks

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
     }
 }
 

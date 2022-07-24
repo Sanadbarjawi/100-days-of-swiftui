8842524e2c992e48c36cd66b49a676885e0d0070
Sanad Barjawi <sanad.barjawi@careem.com>
2022-07-24 17:19:46 +0300

    Slight ui modifiers: 1- Flags shadow and capsule shape 2- Fonts & gradient background

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,17 +14,22 @@ struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland","Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswers: Int = 0
     
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
+            LinearGradient(colors: [.blue, .black],
+                           startPoint: .top,
+                           endPoint: .bottom)
+            .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { index in
                     Button {
@@ -32,20 +37,23 @@ struct ContentView: View {
                     } label: {
                         Image(countries[index])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
                     }
                 }
             }
         }
-        alert(scoreTitle, isPresented: $showingScore) {
+        .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ??")
+            Text("Your score is \(correctAnswers)")
         }
     }
     
     func flagTapped(using number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            correctAnswers += 1
         } else {
             scoreTitle = "Wrong"
         }

1f58cd57e22e6bb692f0d19b4055dbfd8c2fa1b3
Sanad Barjawi <sanad.barjawi@careem.com>
2022-07-24 17:01:39 +0300

    adding score logic

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland","Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland","Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -24,7 +28,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { index in
                     Button {
-                        
+                        flagTapped(using: index)
                     } label: {
                         Image(countries[index])
                             .renderingMode(.original)
@@ -32,6 +36,25 @@ struct ContentView: View {
                 }
             }
         }
+        alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ??")
+        }
+    }
+    
+    func flagTapped(using number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 

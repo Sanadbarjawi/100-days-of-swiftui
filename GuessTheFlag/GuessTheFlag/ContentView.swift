bfb35a61901951aaab4c9dbd65259300246aee3f
Sanad Barjawi <sanad.barjawi@careem.com>
2022-07-31 18:11:01 +0300

    guess the flag challenge

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -52,7 +52,7 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(correctAnswers)")
                     .font(.title.bold())
                     .foregroundColor(.white)
                 Spacer()

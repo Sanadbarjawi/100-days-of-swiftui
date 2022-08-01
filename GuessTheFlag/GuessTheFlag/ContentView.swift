678f13400a776e7ecb7425d0cf5772638496439b
Sanad Barjawi <Sanadbarj@gmail.com>
2022-08-01 19:22:39 +0300

    Project 2 Guess the flag  (#4)
    * Stacks
    
    * Project initiation
    
    * adding score logic
    
    * Slight ui modifiers:
    1- Flags shadow and capsule shape
    2- Fonts & gradient background
    
    * face lifting
    
    * guess the flag challenge
    
    * adding selected answer in the dialog, when user answers wrong
    
    * completing guess the flag challenges
    
    Co-authored-by: Sanad Barjawi <sanad.barjawi@careem.com>

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,24 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland","Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var correctAnswers: Int = 0
+    @State private var selectedAnswer: Int = 0
+    
+    //user gets asked only 8 questions
+    @State private var numberofQuestions: Int = 8
+    @State private var currentQuestionIndex: Int = 1
+    
     var body: some View {
-        VStack {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
+        ZStack {
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45),
+                                         location: 0.3),
+                                   .init(color: Color(red: 0.76, green: 0.15, blue: 0.26),
+                                                                location: 0.3)], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { index in
+                        Button {
+                            flagTapped(using: index)
+                        } label: {
+                            Image(countries[index])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: \(correctAnswers)")
+                    .font(.title.bold())
+                    .foregroundColor(.white)
+                Text("Question: \(currentQuestionIndex)")
+                    .font(.title.bold())
+                    .foregroundColor(.white)
+                Spacer()
             }
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
+            .padding()
         }
+            
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(correctAnswers)")
+        }
+    }
+    
+    func flagTapped(using number: Int) {
+        selectedAnswer = number
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            correctAnswers += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[selectedAnswer])"
+        }
+        showingScore = true
+        
+        if currentQuestionIndex == numberofQuestions {//user reached 8 questions, reset game.
+            correctAnswers = 0
+            currentQuestionIndex = 0
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        currentQuestionIndex+=1
     }
 }
 
--- WeSplit/WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -11,7 +11,28 @@ import SwiftUI
 struct WeSplitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SurveyForm(person: .init(name: "Sam", age: 30))
+        }
+    }
+}
+
+struct Person {
+    var name: String
+    var age: Int
+}
+
+struct SurveyForm: View {
+    @State var person: Person
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("username", text: $person.name)
+                TextField("age", value: $person.age, format: .number)
+            }
+            Section {
+                Text("name is \(person.name), age is \(person.age)")
+            }
         }
     }
 }

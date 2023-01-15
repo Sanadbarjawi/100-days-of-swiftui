75ce3dffeb99b2af5965b82fac9dd843d1a648b8
Sanad <sanad_personal@careemadmins-MacBook-Pro.local>
2023-01-15 19:26:43 +0300

    init better rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sanad on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Sanad on 15/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount: Double = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours",
+                value: $sleepAmount, in: 4...12,
+                step: 0.25)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

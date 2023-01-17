59ee22dd3e2e1996c858f45cdfe5b339a67a75f1
Sanad <sanadbarj@gmail.com>
2023-01-17 20:51:33 +0300

    - adding a date picker - wrapping in a vstack

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount: Double = 8.0
+    @State private var wakeUp: Date = Date.now
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours",
-                value: $sleepAmount, in: 4...12,
-                step: 0.25)
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount, in: 4...12,
+                    step: 0.25)
+            DatePicker("Please enter a time",
+                       selection: $wakeUp,
+                       in: Date.now...,
+                       displayedComponents: .hourAndMinute)
+        }
+        .padding(.all)
     }
 }
 

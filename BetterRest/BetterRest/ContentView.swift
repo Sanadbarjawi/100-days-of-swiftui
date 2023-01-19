b6f7eb5d344dff0c61c781dd5bd4f21f933b2cb3
Sanad <sanadbarj@gmail.com>
2023-01-19 22:12:12 +0300

    initial implementation

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,18 +10,43 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount: Double = 8.0
     @State private var wakeUp: Date = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount, in: 4...12,
-                    step: 0.25)
-            DatePicker("Please enter a time",
-                       selection: $wakeUp,
-                       in: Date.now...,
-                       displayedComponents: .hourAndMinute)
+        
+        NavigationView {
+            VStack(alignment: .center) {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time",
+                           selection: $wakeUp,
+                           in: Date.now...,
+                           displayedComponents: .hourAndMinute)
+                .labelsHidden()
+                
+                Text("Desired amount to sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount, in: 4...12,
+                        step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper( coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                         value: $coffeeAmount, in: 1...12,
+                         step: 1)
+            }
+            .padding(.all)
+            .navigationTitle("App")
+            .toolbar {
+                Button("Calculate") {
+                    
+                }
+            }
         }
-        .padding(.all)
     }
 }
 
